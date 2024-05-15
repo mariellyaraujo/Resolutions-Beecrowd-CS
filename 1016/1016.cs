0af8ec34fbf86ef581936ec0260a34283cd460e9
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2024-05-15 12:20:40 -0300

    Create 1016.cs

---
+++ 1016/1016.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+    namespace beecrowd {
+    class URI1016 {
+
+    public static void Main (string[] args) {
+
+      int y = int.Parse(Console.ReadLine());
+
+      Console.WriteLine($"{y * 2} minutos");
+    }
+  }
+}
