242c454661be43f6ae8e75b44116b9d188a60d8d
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2024-05-15 13:21:31 -0300

    Create 1017

---
+++ 1017/1017.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+    namespace beecrowd {
+    class URI1017 {
+
+    public static void Main (string[] args) {
+
+      double t = double.Parse(Console.ReadLine());
+      double v = double.Parse(Console.ReadLine());
+
+      double d = (t * v) / 12;
+
+      Console.WriteLine($"{d:0.000}");
+    }
+  }
+}
