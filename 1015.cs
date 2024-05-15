8b94b0fdd08b5848de690e2e9e0b906acc487c6f
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2024-05-14 23:11:46 -0300

    Update 1015
    mudança do nome da variável

---
+++ 1015.cs
@@ -0,0 +1,25 @@
+using System;
+using static System.Math;
+
+class Program {
+  public static void Main (string[] args) {
+    
+    string[] ent1 = Console.ReadLine().Split(' ');
+    string[] ent2 = Console.ReadLine().Split(' ');
+
+    double x1 = double.Parse(ent1[0]);
+    double y1 = double.Parse(ent1[1]);
+    double x2 = double.Parse(ent2[0]);
+    double y2 = double.Parse(ent2[1]);
+
+    double s1 = Math.Pow((x2-x1),2);
+    double s2 = Math.Pow((y2-y1),2);
+
+      double z = s1+s2;
+
+    
+    double result = (Math.Sqrt(z));
+
+    Console.WriteLine($"{result:0.0000}");
+  }
+}
