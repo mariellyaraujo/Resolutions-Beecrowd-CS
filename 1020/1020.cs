af9db112e6486b978ee38dfecd07f5fd6794addd
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2024-05-16 19:19:39 -0300

    Create 1020.cs

---
+++ 1020/1020.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+    namespace beecrowd {
+    class URI1019 {
+
+    public static void Main (string[] args) {
+         
+         int valor, valorFinal, anos, meses, dias;
+
+         valor = int.Parse(Console.ReadLine());
+        valorFinal = valor;
+
+           anos = valorFinal / 365;
+           valorFinal -= anos * 365;
+            Console.WriteLine($"{anos} ano(s)");
+
+           meses = valorFinal / 30;
+           valorFinal -= meses * 30;
+            Console.WriteLine($"{meses} mes(es)");
+
+           dias = valorFinal;
+            Console.WriteLine($"{dias} dia(s)");
+
+
+     
+
+    }
+  }
+}
