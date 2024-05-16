124e13f572925c4c87967e8a1b06d9abebea0b44
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2024-05-15 21:41:49 -0300

    Create 1019.cs

---
+++ 1019/1019.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+    namespace beecrowd {
+    class URI1019 {
+
+    public static void Main (string[] args) {
+         
+         int valor, valorFinal, horas, minutos, segundos;
+
+         valor = int.Parse(Console.ReadLine());
+        valorFinal = valor;
+
+           horas = valorFinal / 3600;
+           valorFinal -= horas * 3600;
+
+           minutos = valorFinal / 60;
+           valorFinal -= minutos * 60;
+
+           segundos = valorFinal;
+
+
+     Console.WriteLine($"{horas}:{minutos}:{segundos}");
+
+    }
+  }
+}
