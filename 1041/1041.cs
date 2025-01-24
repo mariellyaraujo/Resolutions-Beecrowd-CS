a0c489bc7752a66c0b4304771c9305807adc3de5
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-01-23 22:50:56 -0300

    tentativa 1 do problema 1041

---
+++ 1041/1041.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+namespace beecrowd {
+    class URI1041 {
+
+        public static void Main (string[] args) {
+         
+            string[] ent = Console.ReadLine().Split(' ');
+
+            double X = double.Parse(ent[0], CultureInfo.InvariantCulture);
+            double Y = double.Parse(ent[1], CultureInfo.InvariantCulture);
+
+            if (X == 0.0 && Y == 0.0){
+                Console.WriteLine("Origem");
+
+            } else if (X == 0){
+                Console.WriteLine("Eixo X");
+
+            } else if (Y == 0){
+                Console.WriteLine("Eixo Y");
+
+            } else if (X > 0 && Y > 0){
+                Console.WriteLine("Q1");
+                
+            } else if (X < 0 && Y > 0){
+                Console.WriteLine("Q2");
+                
+            } else if (X < 0 && Y < 0){
+                Console.WriteLine("Q3");
+                
+            } else if (X > 0 && Y < 0){
+                Console.WriteLine("Q4");
+                
+            }
+
+            
+        }
+    }
+}
