2e99a4c93d8184faa0e70233c1a7278f044f027f
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-01-23 23:22:12 -0300

    tentativa 1 do problema 1042

--- 1042/1042.cs
+++ 1042/1042.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace beecrowd {
+    class URI1042 {
+
+        public static void Main (string[] args) {
+         
+            List<int> _numerosOrd = new List<int>();
+            List<int> _numerosOrg = new List<int>();
+
+            string[] ent = Console.ReadLine().Split(' ');
+
+            int A = int.Parse(ent[0]);
+            int B = int.Parse(ent[1]);
+            int C = int.Parse(ent[2]);
+
+            _numerosOrd.Add(A);
+            _numerosOrd.Add(B);
+            _numerosOrd.Add(C);
+
+            _numerosOrg.Add(A);
+            _numerosOrg.Add(B);
+            _numerosOrg.Add(C);
+
+            _numerosOrd.Sort();
+
+            foreach (var itemOrdenado in _numerosOrd){
+                Console.WriteLine(itemOrdenado);
+            }
+
+            Console.WriteLine();
+
+            foreach (var itemOrdenado in _numerosOrg){
+                Console.WriteLine(itemOrdenado);
+            }
+        }
+    }
+}
