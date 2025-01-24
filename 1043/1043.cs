7f9fc3a772bf920147c7b363933e66dd1faf43a4
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-01-24 11:02:15 -0300

    tentativa 1

--- 1043/1043.cs
+++ 1043/1043.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+
+namespace beecrowd {
+    class URI1040 {
+
+        public static void Main (string[] args) {
+         
+            string[] ent = Console.ReadLine().Split(' ');
+
+            double triangulo, trapezio;
+            double A = double.Parse(ent[0], CultureInfo.InvariantCulture);
+            double B = double.Parse(ent[1], CultureInfo.InvariantCulture);
+            double C = double.Parse(ent[2], CultureInfo.InvariantCulture);
+
+            if ((A + B) >= C) {
+                Console.WriteLine($"triangulo");
+
+            } else {
+                Console.WriteLine($"trapezio");
+            }
+
+        }
+    }
+}
