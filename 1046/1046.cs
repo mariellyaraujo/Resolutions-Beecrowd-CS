71dcc503d20147539676cf14d4683711e6bdcada
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-01-24 18:02:50 -0300

    tentativa 1

--- 1046/1046.cs
+++ 1046/1046.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+namespace beecrowd {
+    class URI1042 {
+
+        public static void Main (string[] args) {
+
+            string[] ent = Console.ReadLine().Split(' ');
+
+            int A = int.Parse(ent[0]);
+            int B = int.Parse(ent[1]);
+
+            if (A > B){
+
+                A = A + 24;
+                Console.WriteLine(A-B);
+
+            } else {
+                Console.WriteLine(A-B);
+            }
+
+        }
+    }
+}
