e46186aea813d710bd8fb79c5df176d30677635c
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-01-24 12:05:02 -0300

    tentativa 1

--- 1045/1045.cs
+++ 1045/1045.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace beecrowd {
+    class URI1040 {
+
+        public static void Main(string[] args) {
+
+            List<double> lista = new List<double>();
+            double A, B, C;
+
+            string[] numeros = Console.ReadLine().Split(' ');
+            foreach (var numero in numeros) {
+                lista.Add(double.Parse(numero));
+            }
+            lista.Sort((x, y) => y.CompareTo(x));
+
+            A = lista[0];
+            B = lista[1];
+            C = lista[2];
+
+            if ((B+C) <= A){
+                Console.WriteLine("nao eh tiangulo");
+
+            } else {
+                Console.WriteLine(" eh tiangulo");
+            }
+
+        }
+    }
+}
