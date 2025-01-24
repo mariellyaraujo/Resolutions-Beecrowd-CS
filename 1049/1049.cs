9ca080e85b0e3aedac6904516cd24fcabde32138
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-01-24 19:22:42 -0300

    tentativa q

---
+++ 1049/1049.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace beecrowd {
+    class URI1049 {
+
+        public static void Main(string[] args) {
+
+            string A = Console.ReadLine();
+            string B = Console.ReadLine();
+            string C = Console.ReadLine();
+
+            if (A == "vertebrado") {
+
+                if (B == "ave") {
+
+                    if (C == "carnivoro") {
+                        Console.WriteLine("aguia");
+
+                    } else if (C == "onivoro") {
+                        Console.WriteLine("pomba");
+                    }
+
+                } else if (B == "mamifero") {
+
+                    if (C == "onivoro") {
+                        Console.WriteLine("homem");
+
+                    } else if (C == "herbivoro") {
+                        Console.WriteLine("vaca");
+                    }
+                    
+
+                }
+
+            } else if (A == "invertebrado") {
+                
+                if (B == "inseto") {
+
+                    if (C == "hematofago") {
+                        Console.WriteLine("pulga");
+
+                    } else if (C == "herbivoro") {
+                        Console.WriteLine("lagarta");
+                    }
+
+                } else if (B == "anelideo") {
+
+                    if (C == "hematofago") {
+                        Console.WriteLine("pulga");
+
+                    } else if (C == "onivoro") {
+                        Console.WriteLine("minhoca");
+                    }
+                    
+
+                }
+            }
+        }
+    }
+}
