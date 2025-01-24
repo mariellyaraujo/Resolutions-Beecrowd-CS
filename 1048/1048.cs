49e78ae8ad2d14640a937fad1d20417c6387d6d8
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-01-24 18:54:32 -0300

    tentativa 1

--- 1048/1048.cs
+++ 1048/1048.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+
+namespace beecrowd {
+    class URI1048 {
+
+        public static void CalcPorcent(double s, double p) {
+
+            p = (s * p) / 100;
+            s = s + p;
+
+            Console.WriteLine($"Novo salario: {s}");
+            Console.WriteLine($"Reajuste ganho: {p}");
+        }
+        public static void Main (string[] args) {
+
+            double porcent;
+            double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            if(salario >= 0 && salario <= 400.00) {
+                porcent = 15;
+                CalcPorcent(salario, porcent);
+                Console.WriteLine($"Em percentual: {porcent} %");
+
+            } else if(salario >= 400.01 && salario <= 800.00) {
+                porcent = 12;
+                CalcPorcent(salario, porcent);
+                Console.WriteLine($"Em percentual: {porcent} %");
+
+            } else if(salario >= 800.01 && salario <= 1200.00) {
+                porcent = 10;
+                CalcPorcent(salario, porcent);
+                Console.WriteLine($"Em percentual: {porcent} %");
+
+            } else if(salario >= 1200.01 && salario <= 2000.00) {
+                porcent = 7;
+                CalcPorcent(salario, porcent);
+                Console.WriteLine($"Em percentual: {porcent} %");
+
+            } else if(salario > 2000.00) {
+                porcent = 4;
+                CalcPorcent(salario, porcent);
+                Console.WriteLine($"Em percentual: {porcent} %");
+
+            }
+
+
+        }
+    }
+}
