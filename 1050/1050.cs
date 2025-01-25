0a983213fda4434185fe69c2106bbdb891f1d550
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-01-25 19:58:07 -0300

    tentativa 1

--- 1050/1050.cs
+++ 1050/1050.cs
@@ -7,7 +7,25 @@ namespace beecrowd {
 
             int A = int.Parse(Console.ReadLine());
             
-            
+            if( A == 61) {
+                Console.WriteLine("Brasilia");
+
+            } else if (A == 71){
+                Console.WriteLine("Salvador");
+
+            } else if (A == 11){
+                Console.WriteLine("Sao Paulo");
+                
+            } else if (A == 21){
+                Console.WriteLine("Rio de Janeiro");
+                
+            } else if (A == 32){
+                Console.WriteLine("Juiz de Fora");
+                
+            } else if (A == 19){
+                Console.WriteLine("Cp");
+                
+            }
         }
     }
 }
