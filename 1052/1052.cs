4dddf45dadade3c4e0035a3ac3df8e9354c3f610
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-01-27 18:39:24 -0300

    resolução 1052

--- 1052/1052.cs
+++ 1052/1052.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace beecrowd {
+    class URI1050 {
+
+        public static void Main(string[] args) {
+
+            int A = int.Parse(Console.ReadLine());
+            
+            if( A == 1) {
+                Console.WriteLine("January");
+
+            } else if (A == 2){
+                Console.WriteLine("February");
+
+            } else if (A == 3){
+                Console.WriteLine("March");
+                
+            } else if (A == 4){
+                Console.WriteLine("April");
+                
+            } else if (A == 5){
+                Console.WriteLine("May");
+                
+            } else if (A == 6){
+                Console.WriteLine("June");
+                
+            }else if (A == 7){
+                Console.WriteLine("July");
+                
+            }else if (A == 8){
+                Console.WriteLine("August");
+                
+            } else if (A == 9){
+                Console.WriteLine("September");
+                
+            } else if (A == 10){
+                Console.WriteLine("October");
+                
+            }else if (A == 11){
+                Console.WriteLine("November");
+                
+            }else if (A == 12){
+                Console.WriteLine("December");
+                
+            } 
+        }
+    }
+}
