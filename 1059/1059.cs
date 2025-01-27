397f06bbe76a2a19ebf685fcdf45905ea7abdf3a
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-01-27 18:45:22 -0300

    Resolução 1059

---
+++ 1059/1059.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace beecrowd {
+    class URI1059 {
+
+        public static void Main(string[] args) {
+
+           for (int i = 2; i < 101; i++) {
+           
+            Console.WriteLine(i);
+            i++;
+
+            }
+        }
+    }
+}
