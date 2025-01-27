d6cd0fcf1f0125821fe256e878f4b3ef0fd7e49c
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-01-27 20:55:53 -0300

    teste

--- 1064/1064.cs
+++ 1064/1064.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+
+namespace beecrowd {
+    class URI1060 {
+
+        public static void Main(string[] args) {
+
+        double num, soma;
+        int positivos = 0;
+
+        for (int i = 0; i < 6; i++) {
+        
+            num = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+            if (num >= 0) {
+                soma += num;
+                positivos++;
+            }
+
+        }
+
+        Console.WriteLine($"{positivos} valores positivos");
+        Console.WriteLine($"{soma} valores positivos");
+        }
+    }
+}
