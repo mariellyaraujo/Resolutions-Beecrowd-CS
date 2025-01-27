95d6200a7f6c6b4c54691154f05556cd132d7194
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-01-27 20:28:45 -0300

    ajustes

---
+++ 1061/1061.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace beecrowd {
+    class URI1061 {
+
+        public static void Main(string[] args) {
+
+            string[] ent = Console.ReadLine().Split(' ');
+
+            Console.WriteLine(ent[0]);
+            Console.WriteLine(ent[1]);
+            /*string[] ent = Console.ReadLine().Split(' ');
+
+            int horaInicio = int.Parse(ent[0]);
+            int minutoInicio = int.Parse(ent[1]);
+            int horaFim = int.Parse(ent[2]);
+            int minutoFim = int.Parse(ent[3]);
+
+            int duracaoHora;
+            int duracaoMinuto;
+
+            int minutosInicio = horaInicio * 60 + minutoInicio;
+            int minutosFim = horaFim * 60 + minutoFim;
+
+            if (minutosFim <= minutosInicio) {
+                minutosFim += 24 * 60; 
+            }
+
+            int duracaoTotalMinutos = minutosFim - minutosInicio;
+
+          
+            duracaoHora = duracaoTotalMinutos / 60;
+            duracaoMinuto = duracaoTotalMinutos % 60;
+
+            Console.WriteLine($"O JOGO DUROU {duracaoHora} HORA(S) E {duracaoMinuto} MINUTO(S)");*/
+        }
+    }
+}
