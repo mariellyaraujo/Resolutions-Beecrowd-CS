11f1d4aa0e20fba1c686adebcf276b65e9fd6041
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-01-27 18:33:24 -0300

    Resolução 1051

--- 1051/1051.cs
+++ 1051/1051.cs
@@ -4,43 +4,25 @@ using System.Globalization;
 namespace beecrowd {
     class URI1051 {
 
-        public static void Main (string[] args) {
+        public static void Main(string[] args) {
 
-            double resto, porcento;
             double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double imposto = 0.0;
 
             if (salario >= 0 && salario <= 2000.00) {
                 Console.WriteLine("Isento");
 
             } else if (salario >= 2000.01 && salario <= 3000.00) {
-                resto = salario - 2000.00;
-                porcento = (resto * 8) / 100;
-                Console.WriteLine($"R$ {porcento:0.00}");
+                imposto = (salario - 2000.00) * 0.08;
+                Console.WriteLine($"R$ {imposto:0.00}");
 
             } else if (salario >= 3000.01 && salario <= 4500.00) {
+                imposto = (1000.00 * 0.08) + ((salario - 3000.00) * 0.18);
+                Console.WriteLine($"R$ {imposto:0.00}");
                 
-                resto = 1000.00;
-                porcento = (resto * 8) / 100;
-                
-                salario += -3000.00;
-
-                porcento += (salario*18) / 100;
-                Console.WriteLine($"R$ {porcento:0.00}");
-
             } else {
-                
-                resto = 1000.00;
-                porcento = (resto * 8) / 100;
-
-                resto = 1500.00;
-     
-                porcento += (resto*18) / 100;
-
-                salario += -4.500;
-
-                porcento += (salario*28) / 100;
-
-                Console.WriteLine($"R$ {porcento:0.00}");
+                imposto = (1000.00 * 0.08) + (1500.00 * 0.18) + ((salario - 4500.00) * 0.28);
+                Console.WriteLine($"R$ {imposto:0.00}");
             }
         }
     }
--- 1051/obj/Debug/net8.0/1051.AssemblyInfo.cs
+++ 1051/obj/Debug/net8.0/1051.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("1051")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+338d57f75d3a08626cbc61d2f4ae5f59cd99b003")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed2b3f5c83baf45410c570b533b7480c6c0c9cc6")]
 [assembly: System.Reflection.AssemblyProductAttribute("1051")]
 [assembly: System.Reflection.AssemblyTitleAttribute("1051")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
