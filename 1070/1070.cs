a40e03d9f51ce9732c084ca417b16ccee58c68d6
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-01-27 21:36:19 -0300

    criação da pasta 1070

---
+++ 1070/1070.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace beecrowd {
+    class URI1070 {
+
+        public static void Main (string[] args) {
+
+            int y = int.Parse(Console.ReadLine());
+            
+
+            for (int i = 1; i <= y; i++) {
+        
+            Console.WriteLine(i);
+            i++;
+
+        }
+
+        }
+    }
+}
