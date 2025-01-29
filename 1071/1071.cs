ae41c9402949382ad9b7716f17eac2f6ed747820
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-01-29 14:34:05 -0300

    teste

--- 1071/1071.cs
+++ 1071/1071.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+
+namespace beecrowd {
+    class URI1067 {
+
+        public static void Main (string[] args) {
+
+            int soma = 0;
+            int y = int.Parse(Console.ReadLine());
+            int x = int.Parse(Console.ReadLine());
+            
+
+            for (int i = x; i <= y; i++) {
+        
+            if (i % 2 == 1) {
+                soma += i;
+            }
+
+        }
+            Console.WriteLine(soma);
+
+        }
+    }
+}
