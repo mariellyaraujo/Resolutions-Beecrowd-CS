0bb31acfed73fbde75a888e6a785593e6d501a37
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-01-29 15:19:55 -0300

    Resolução 1072

--- 1072/1072.cs
+++ 1072/1072.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+
+namespace beecrowd {
+    class URI1072 {
+
+        public static void Main (string[] args) {
+
+            int iin = 0;
+            int oout = 0;
+            int y = int.Parse(Console.ReadLine());
+
+            
+            
+
+            for (int i = 0; i < y; i++) {
+        
+            int x = int.Parse(Console.ReadLine());
+
+            if (x >= 10 && x <= 20) {
+                iin++;
+            } else {
+                oout++;
+            }
+
+        }
+            Console.WriteLine($"{iin} in");
+            Console.WriteLine($"{oout} out");
+
+        }
+    }
+}
