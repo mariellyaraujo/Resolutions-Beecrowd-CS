7bf0c8931add374798b73e3b2a55483dfa8c52d5
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-01-29 15:47:35 -0300

    Resolução 1075

---
+++ 1075/1075.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace beecrowd {
+    class URI1075 {
+
+        public static void Main (string[] args) {
+
+            int resto;
+            int y = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 10000; i++) {
+            resto = i / y;
+
+            if (i % y == 2) {
+                Console.WriteLine(i);
+            }
+
+        }
+
+        }
+    }
+}
