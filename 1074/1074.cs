96f9ade5ad9f1cdc4a8479e921965e10624c0f99
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-01-29 15:39:59 -0300

    Resolução 1074

--- 1074/1074.cs
+++ 1074/1074.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+
+namespace beecrowd {
+    class URI1074 {
+
+        public static void Main (string[] args) {
+
+            int x;
+            int y = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= y; i++) {
+        
+            x = int.Parse(Console.ReadLine());
+
+            if (x == 0 ) {
+                Console.WriteLine($"NULL");
+            } else if (x % 2 == 0 && x > 0) {
+                Console.WriteLine($"EVEN POSITIVE");
+            } else if (x % 2 == 0 && x < 0) {
+                Console.WriteLine($"EVEN NEGATIVE");
+            } else if (x % 2 != 0 && x > 0) {
+                Console.WriteLine($"ODD POSITIVE");
+            } else if (x % 2 != 0 && x < 0) {
+                Console.WriteLine($"ODD NEGATIVE");
+            }
+
+        }
+
+        }
+    }
+}
