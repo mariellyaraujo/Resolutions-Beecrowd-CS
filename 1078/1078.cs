e7f0df5f2cd35efe829d605aa860cbaf0a4940ed
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-01-29 15:55:38 -0300

    Resolução 1078

---
+++ 1078/1078.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace beecrowd {
+    class URI1078 {
+
+        public static void Main (string[] args) {
+
+            int y = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++) {
+ 
+            Console.WriteLine($"{i} x {y} = {y*i}");
+
+        }
+
+        }
+    }
+}
