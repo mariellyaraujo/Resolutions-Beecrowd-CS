eb447daabe1717b6ab66bdd4133eb3dc5c62b14b
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-02-17 23:56:32 -0300

    resolução 1080

--- 1080/1080.cs
+++ 1080/1080.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+
+ namespace beecrowd {
+    class URI1080 {
+   
+     public static void Main(string[] args)
+     {
+       int maiorNum = 0;
+       int posicao = 0;
+       
+       for(int i = 1; i <= 100; i++) {
+         
+         int num = int.Parse(Console.ReadLine());
+         
+         if(num > maiorNum) {
+           
+           maiorNum = num;
+           posicao = i;
+           
+         }
+       }
+       
+       Console.WriteLine(maiorNum);
+       Console.WriteLine(posicao);
+       
+       
+     }
+   }
+ }
