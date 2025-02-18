e16e2bdcf7feaff10dadb4b6dc0a629a10bb96df
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-02-17 23:54:48 -0300

    criação da pasta 1080

--- 1016/obj/Debug/net8.0/1016.AssemblyInfo.cs
+++ 1016/obj/Debug/net8.0/1016.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("1016")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11d372eb82c09a124334c80a9756ba4058584880")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a56cfabc603ccecebbddafa1577b65beeefe7c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("1016")]
 [assembly: System.Reflection.AssemblyTitleAttribute("1016")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
