8fe3849c63506afecb641efdf81d2879232ff141
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-03-08 10:51:49 -0300

    update

--- 1094/1094.cs
+++ 1094/1094.cs
@@ -8,12 +8,26 @@ namespace beecrowd {
             int num = int.Parse(Console.ReadLine());
 
             for(int i = 0; i < num; i++){
-                entrada = Console.ReadLine().Split(' ');
+                string[] entrada = Console.ReadLine().Split(' ');
 
                 int quant = int.Parse(entrada[0]);
-                string tipo = entrada[1];
+                char tipo = char.Parse(entrada[1]);
 
-                
+                switch(tipo){
+
+                    case 'C':
+                        coelhosN =+ quant;
+                        break;
+
+                    case 'S':
+                        saposN =+ quant;
+                        break;
+                    
+                    case 'R':
+                        ratosN =+ quant;
+                        break;
+                }
+
             }
         }
     }
