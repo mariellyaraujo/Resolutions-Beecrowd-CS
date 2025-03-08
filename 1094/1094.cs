d18c394c186429a1466c620366e1db357314b5ae
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-03-08 10:42:36 -0300

    update

--- 1094/1094.cs
+++ 1094/1094.cs
@@ -4,7 +4,12 @@ namespace beecrowd {
     class URI1094 {
 
         public static void Main (string[] args) {
+            int coelhosN, saposN, ratosN;
+            int num = int.Parse(Console.ReadLine());
 
+            for(int i = 0; i < num; i++){
+                
+            }
         }
     }
 }
