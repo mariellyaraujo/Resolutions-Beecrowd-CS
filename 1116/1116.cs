d0f3be605edd43eb1d238be980e8331bf6c5319c
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2026-08-11 13:55:39 -0300

    resolução do problema 1116

---
+++ 1116/1116.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace beecrownd
+{
+    class URI1116
+    {
+        public static void Main(string[] args)
+        {
+            string linha = Console.ReadLine();
+
+            int ent = int.Parse(linha);
+
+            for (int i = 0; i < ent; i++)
+            {
+                string[] entrada = Console.ReadLine().Split(' ');
+
+                int a = int.Parse(entrada[0]);
+                int b = int.Parse(entrada[1]);
+
+                if (b == 0)
+                {
+                    Console.WriteLine("divisao impossivel");
+                }
+                else
+                {
+                    double result = (double)a / b;
+                    Console.WriteLine(result.ToString("F1", CultureInfo.InvariantCulture));
+                }
+            }
+        }
+    }
+}
