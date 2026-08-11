a20ef0c9ab92dc3bd2a984bfcfb8da686e82e6b7
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2026-08-11 14:15:26 -0300

    resolução do problema 1117

---
+++ 1117/1117.cs
@@ -0,0 +1,32 @@
+﻿using System; 
+using System.Globalization;
+
+namespace beecrownd {
+    class URI1117 {
+
+        static void Main(string[] args) { 
+
+           int i = 0;
+           double media = 0.0;
+
+           while(i < 2)
+            {
+                string input = Console.ReadLine(); 
+                double entrada = double.Parse(input, CultureInfo.InvariantCulture);
+
+                if (entrada <= 0 || entrada > 10)
+                {
+                    Console.WriteLine("nota invalida");
+                } else
+                {
+                    media = media + entrada;
+                    i++;
+                }
+            }
+            media = media / 2;
+            Console.WriteLine($"media = {(media.ToString("F2", CultureInfo.InvariantCulture))}");
+
+        }
+
+    }
+}
