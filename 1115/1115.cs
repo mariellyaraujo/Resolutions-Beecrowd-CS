9ef4962e407ba22bbcb3eb09f4cccc2609c36082
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2026-08-11 13:23:07 -0300

    resolução do problema 1115

---
+++ 1115/1115.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace beecrownd
+{
+    class URI1115
+    {
+        public static void Main (String[] args){
+            string result = "";
+            while (true)
+            {
+                string[] entrada = Console.ReadLine().Split(' ');
+
+                int a = int.Parse(entrada[0]);
+                int b = int.Parse(entrada[1]);
+
+                if (a == 0 || b == 0) break;
+
+                if(a > 0 && b > 0) result = "primeiro";
+                else if(a < 0 && b > 0) result = "segundo";
+                else if(a < 0 && b < 0) result = "terceiro";
+                else if(a > 0 && b < 0) result = "quarto";
+
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
