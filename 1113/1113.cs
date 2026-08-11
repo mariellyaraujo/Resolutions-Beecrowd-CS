df01e4a712d6977ab3bff6036b49f781d3956b19
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2026-08-11 10:07:23 -0300

    resolução problema 1113

---
+++ 1113/1113.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace beecrownd
+{
+    class URI1113
+    {
+        public static void Main (String[] args)
+        {
+            while (true)
+            {
+                string[] entrada = Console.ReadLine().Split(' ');
+
+                int a = int.Parse(entrada[0]);
+                int b = int.Parse(entrada[1]);
+
+                if (a == b) {
+                    break;
+                } else if (a < b) {
+                    Console.WriteLine("Crescente");
+                } else if (b < a) {
+                    Console.WriteLine("Decrescente");
+                }
+            }
+        }
+    }
+}
