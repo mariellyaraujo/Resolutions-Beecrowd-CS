7a8c2384e0b98c87c08f8f08da4661d3051e132a
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2026-08-11 10:23:25 -0300

    resolução do problema 1095

---
+++ 1095/1095.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace beecrownd
+{
+    class URI1095
+    {
+        public static void Main (String[] args)
+        {
+            int a = 1, b = 60;
+            for (; b >= 0; b -= 5, a += 3)
+            {
+                Console.WriteLine($"I={a} J={b}");
+            }
+        }
+    }
+}
