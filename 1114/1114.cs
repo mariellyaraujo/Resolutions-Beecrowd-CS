f9c941da0e0eb1b58866414a1c724360ba179394
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2026-08-11 10:33:22 -0300

    resolução do problema 1114

---
+++ 1114/1114.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace beecrownd
+{
+    class URI1114
+    {
+        public static void Main (String[] args)
+        {
+            while (true)
+            {
+                int entrada = int.Parse(Console.ReadLine());
+
+                if (entrada != 2002)
+                {
+                    Console.WriteLine("Senha Invalida");
+                } else
+                {
+                    Console.WriteLine("Acesso Permitido");
+                    break;
+                }
+            }
+        }
+    }
+}
