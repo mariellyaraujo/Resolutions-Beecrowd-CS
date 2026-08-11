947745aae9509d04be518ecbe4bb7f76b2c109b7
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2026-08-11 09:28:10 -0300

    update

---
+++ 1099/1099.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace beecrownd
+{
+    class URI1099{
+        public static void Main (String[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < num; i++)
+            {
+                string[] entrada = Console.ReadLine().Split(' ');
+
+                int x = int.Parse(entrada[0]);
+                char y = int.Parse(entrada[1]);
+            }
+        }
+    }
+}
