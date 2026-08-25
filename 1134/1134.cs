17d8ae8ab47a33aac158f7f1495642e0baeaeb0e
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2026-08-25 15:44:55 -0300

    resolução 1134

---
+++ 1134/1134.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace beecrownd
+{
+    class URI1134
+    {
+        public static void Main(string[] args)
+        {
+            int a = 0, g = 0, d = 0;
+            bool continuar = true;
+            while (continuar)
+            {
+                int x = int.Parse(Console.ReadLine());
+
+                switch (x)
+                {
+                    case 1:
+                        a++;
+                        break;
+
+                    case 2:
+                        g++;
+                        break;
+
+                    case 3:
+                        d++;
+                        break;
+
+                    case 4:
+                        Console.WriteLine($"MUITO OBRIGADO\nAlcool: {a}\nGasolina: {g}\nDiesel: {d}");
+                        continuar = false;
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+
+}
