5ebc22e5f9fb7cb521803490a145ee607ddc0d8f
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2026-08-25 08:35:04 -0300

    resolução 1117

--- 1117/1117.cs
+++ 1117/1117.cs
@@ -3,8 +3,31 @@ using System.Globalization;
 
 namespace beecrownd {
     class URI1117 {
+        public static void calcNotas()
+        {
+            int i = 0;
+            double media = 0.0;
+
+            while(i < 2)
+            {
+                string input = Console.ReadLine(); 
+                double entrada = double.Parse(input, CultureInfo.InvariantCulture);
+
+                if (entrada <= 0 || entrada > 10)
+                {
+                    Console.WriteLine("nota invalida");
+                } else
+                {
+                    media = media + entrada;
+                    i++;
+                }
+            }
+            media = media / 2;
+            Console.WriteLine($"media = {(media.ToString("F2", CultureInfo.InvariantCulture))}");
+        }
+
         public static void Main (string[] args){
-            
+            calcNotas();
         }
     }
 }
