72079e5868bdbd284ed2f247a6fff1d9bd579998
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2026-08-25 10:40:12 -0300

    tent 1

---
+++ 1132/1132.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace beecrownd {
+    class URI1132 {
+        public static void Main(string[] args){
+            int soma = 0;
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            for(int i = a; i < b; i++){
+                if (i % 13 != 0){
+                    soma = soma + i;
+                }
+            }
+
+            Console.WriteLine(soma);
+        }
+    }
+}
