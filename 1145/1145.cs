52c08fbcad2c5e2fa1898d5407f7630ad2f5831e
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2026-09-01 10:55:30 -0300

    resolução 1145

---
+++ 1145/1145.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+namespace beecrownd
+{
+    class URI1145
+    {
+        public static void Main(string[] args)
+        {
+            string[] entrada = Console.ReadLine().Split(' ');
+
+            int a = int.Parse(entrada[0]);
+            int b = int.Parse(entrada[1]);
+
+            int num = 0;
+
+            for(int i = 1; i <= b; i += a)
+            {
+                num = i;
+                for(int j = 0; j < a; j++)
+                {
+                    if (num > b) break;
+
+                    if (j == 0)
+                    {
+                        Console.Write(num + j);
+                    } else
+                    {
+                        Console.Write($" {num + j}");
+                    }
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
