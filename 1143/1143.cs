5a2b3c22d25fcaf128e5f0b529befc470b0ff147
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2026-09-01 09:44:39 -0300

    resolução 1143

---
+++ 1143/1143.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace beecrownd
+{
+    class URI1143
+    {
+        public static void Main(string[] args)
+        {
+            int ent = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i <= ent; i++)
+            {
+                Console.WriteLine($"{i} {i*i} {i*i*i}");
+            }
+        }
+    }
+}
