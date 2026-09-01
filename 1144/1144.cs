15c2af56af0ca8be108843bf337590457fd3044f
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2026-09-01 10:33:37 -0300

    resolução 1144

---
+++ 1144/1144.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace beecrownd {
+    class URI1144 {
+        public static void Main(string[] args) {
+            int ent = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i <= ent; i++)
+            {
+                for(int j = 1; j <= 2; j++)
+                {
+                    if (j == 1)
+                    {
+                        Console.WriteLine($"{i} {i*i} {i*i*i}");
+                    } else
+                    {
+                        Console.WriteLine($"{i} {(i*i)+1} {(i*i*i)+1}");
+                    }
+                    
+                }
+            }
+        }
+    }
+}
