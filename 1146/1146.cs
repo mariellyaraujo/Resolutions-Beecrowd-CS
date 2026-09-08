4ca26ec7c2d4f0b62e1ff6449327d6a88de7230a
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2026-09-08 09:21:25 -0300

    resolução 1146

---
+++ 1146/1146.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace beecrownd
+{
+    class URI1146
+    {
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                int ent = int.Parse(Console.ReadLine());
+
+                if (ent == 0) break;
+
+                for (int i = 1; i <= ent; i++)
+                {
+                    if (i == 1)
+                    {
+                        Console.Write(i);
+                    } else
+                    {
+                        Console.Write($" {i}");
+                    }
+
+                    if (i == ent) Console.WriteLine("");
+                }
+            }
+        }
+    }
+}
